eff801921a80f8279c067fa2ffdb4554119bb47d
Fabrizio Padulano <fabrinapoli19@gmail.com>
2024-03-09 09:09:52 +0100

    [two pointers] Longest Repeating Character Replacement

---
+++ src/main/java/org/padulano/fabrizio/algorithms/twopointers/LongestRepeatingCharacterReplacement.java
@@ -0,0 +1,33 @@
+package org.padulano.fabrizio.algorithms.twopointers;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// https://leetcode.com/problems/longest-repeating-character-replacement/description/
+public class LongestRepeatingCharacterReplacement {
+    public int characterReplacement(String s, int k) {
+        Map<Character, Integer> count = new HashMap<>();
+
+        int l = 0, r = 0, max = 0, result = 0;
+
+        while (r < s.length()) {
+            int size = r - l + 1;
+            int rCount = count.getOrDefault(s.charAt(r), 0) + 1;
+            count.put(s.charAt(r), rCount);
+
+            max = Math.max(max, rCount);
+
+            while (size - max > k) {
+                count.put(s.charAt(l), count.get(s.charAt(l)) - 1);
+                l++;
+                size = r - l + 1;
+            }
+
+            result = Math.max(result, size);
+
+            r++;
+        }
+
+        return result;
+    }
+}
