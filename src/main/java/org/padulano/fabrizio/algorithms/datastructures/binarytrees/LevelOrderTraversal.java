634df01584c1d419d55b60ee7924c46d154b2c66
Fabrizio Padulano <fabrinapoli19@gmail.com>
2024-04-06 08:28:18 +0200

    [binary trees] level order traversal

---
+++ src/main/java/org/padulano/fabrizio/algorithms/datastructures/binarytrees/LevelOrderTraversal.java
@@ -0,0 +1,40 @@
+package org.padulano.fabrizio.algorithms.datastructures.binarytrees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+public class LevelOrderTraversal {
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            while (size > 0) {
+                var node = q.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+
+                if (node.right != null) {
+                    q.add(node.right);
+                }
+
+                size--;
+            }
+
+            res.add(level);
+        }
+
+        return res;
+    }
+}
